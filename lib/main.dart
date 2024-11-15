49620844fa6f1153f236f4a8b1c66d78b731745d
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-11-15 23:07:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Personal Expense',
+      title: 'Personal Expenses',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.teal,
